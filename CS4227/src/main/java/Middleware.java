ff5910b0599c16e4786680afcebc06dce1588ba4
Tom-J-Barrett <motb141@gmail.com>
2017-10-20 11:45:23 +0100

    Add Validation Class

---
+++ CS4227/src/main/java/Middleware.java
@@ -0,0 +1,8 @@
+import requestManagement.RequestManager;
+
+public class Middleware {
+    public static void main(String [] args) {
+        System.out.println("Hi");
+
+    }
+}
---
+++ CS4227/src/main/java/Validation/ValadationService.java
@@ -0,0 +1,15 @@
+package Validation;
+
+import requestManagement.Context;
+import requestManagement.Service;
+
+public class ValadationService implements Service {
+
+    public void processIncomingRequest(Context context) {
+
+    }
+
+    public void processOutgoingResponse(Context context) {
+
+    }
+}
