f2a33c6e6d909c9ad715bd65579fcd3c3e44a7e7
Tom-J-Barrett <motb141@gmail.com>
2017-10-20 13:08:46 +0100

    Validation rule iterator

--- CS4227/src/main/java/Validation/ValidationService.java
+++ CS4227/src/main/java/Validation/ValidationService.java
@@ -3,13 +3,25 @@ package Validation;
 import requestManagement.Context;
 import requestManagement.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationService implements Service {
 
     public void processIncomingRequest(Context context) {
-        ValidationRule validationRule = new HttpValidationRule();
+        validateHttp(context);
     }
 
     public void processOutgoingResponse(Context context) {
+        validateHttp(context);
+    }
 
+    public void validateHttp(Context context) {
+        List<ValidationRule> rules = new ArrayList<ValidationRule>();
+        rules.add(new HttpValidationRule());
+
+        for ( ValidationRule rule : rules){
+            rule.validate(context);
+        }
     }
 }
